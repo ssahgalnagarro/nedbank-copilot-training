d66d6af3ae5c261fa9d391913317b05ed6bfe919
Shubham Sahgal <>
2025-07-03 12:45:41 +0530

    Update siteProps with correct name and email

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Git Hub Co-pilot Training Batch -1",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "dummy@nagarro.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
